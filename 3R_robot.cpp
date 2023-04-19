df5e39fbecbd78a99b22bb7d3c57449c1c1070ce
Arjun Raja <ar6841@nyu.edu>
2023-04-19 18:57:48 -0400

    Added control submodule, added example robot

---
+++ 3R_robot.cpp
@@ -0,0 +1,52 @@
+#include "Dual_Quaternion_Forward_Kinematics/forward_kinematics_dualquaternion.h"
+#include<iostream>
+#include "Dual_Quaternion_Forward_Kinematics/jacobians.h"
+#include"Dual_Quaternion_Control/Dual_Pose_Control.h"
+
+// Order of joint vars {theta, alpha, a, d, type}
+int main()
+{
+    
+
+    double arm_length = 5.0;
+    //Joint 1
+    DH::DH_joint<double> J1(0,0,arm_length,0, 0);
+
+    //Joint 2
+    DH::DH_joint<double> J2(0,0,arm_length,0, 0);
+
+    //Joint 3
+    DH::DH_joint<double> J3(0,0,arm_length,0, 0);
+
+    
+
+    Kinematics::RobotLinks<double> MyRobot;
+
+    MyRobot.addJoint(J1);
+    MyRobot.addJoint(J2);
+    MyRobot.addJoint(J3);
+
+    std::cout<<"\nTest Outputs"<<std::endl;
+    std::cout<<"\n"<<Kinematics::ComputeJacobian(MyRobot)<<std::endl;
+    std::cout<<"\n"<<Kinematics::compute_pose_dot(MyRobot)<<std::endl;
+
+    double total_time = 10, dt = 0.01;
+
+    int N = static_cast<int>(ceil(total_time/dt)); 
+
+    Eigen::Matrix<double,3,1> joint_vec_desired;
+    joint_vec_desired<<90,0,0;
+
+    std::cout<<"\nJoint Vec desired: "<<joint_vec_desired<<std::endl;
+
+    dualquat::DualQuaternion q_desired  = MyRobot.ComputeForwardKinematics(joint_vec_desired);
+
+    Eigen::Matrix<double,3,Eigen::Dynamic> Output(3,N);
+
+    Output = Controller::pose_difference_controller(MyRobot,q_desired,dt,total_time,10.0);
+
+    std::cout<<"\nControl output : "<<Output.col(N-1)<<std::endl;
+
+
+return 0;
+}
